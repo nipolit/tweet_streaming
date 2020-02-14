1cdb49ae48f03ed78eb2189dbe47fa19507782fd
Nikita Politaev <nipolit@gmail.com>
2020-02-14 12:34:33 +0700

    Debug errors in an IDE

--- backend/backend.py
+++ backend/backend.py
@@ -10,3 +10,7 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
